51bfd049739266a53fd7f0d0778673b90419f419
William <cloutier23@up.edu>
2020-10-06 00:26:05 -0700

    Made program functional. Now draws a face and implements many requirements of the assignment

--- app/src/main/java/com/example/homework1/Face.java
+++ app/src/main/java/com/example/homework1/Face.java
@@ -1,42 +1,145 @@
 package com.example.homework1;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
 import androidx.appcompat.app.AppCompatActivity;
-public class Face  extends AppCompatActivity {
+
+import java.util.Random;
+
+public class Face  extends SurfaceView {
+    private Vars varObject;
 
     /**
      * Author: William Cloutier
-     * Date: 9/20/20
-     * Version: Homework A
+     * Date: 10/6/20
+     * Version: Homework B
      */
 
 
-     //public class that has 4 private int vars
-     private int skinColor;
-     private int eyeColor;
-     private int hairColor;
-     private int hairStyle;
+     //Paints for drawing
+     Paint skin = new Paint();
+     Paint eyes = new Paint();
+     Paint hair = new Paint();
 
      //constructor that calls on the randomize methods
-    public Face(){
-        randomize();
+    public Face(Context context, AttributeSet attrs){
+        super(context, attrs);
+        varObject = new Vars();
 
+
+        setWillNotDraw(false);
+
+
+            random();
+            skin.setColor(Color.rgb(varObject.redSkin, varObject.greenSkin, varObject.blueSkin));
+            skin.setStyle(Paint.Style.FILL);
+
+            random();
+            eyes.setColor(Color.rgb(varObject.redEyes, varObject.greenEyes, varObject.blueEyes));
+            eyes.setStyle(Paint.Style.FILL);
+
+            random();
+            hair.setColor(Color.rgb(varObject.redHair, varObject.greenHair, varObject.blueHair));
+            hair.setStyle(Paint.Style.FILL);
+
+
+
+        setBackgroundColor(Color.WHITE);  //better than black default
     }
 
-    //void method that sets random values to the private vars. The firs three values go to 255
-    //They go to 255 because of rgb and the last one only goes to 3 total values, since there are 3 hairstyles
-    public void randomize()
-    {
-        int randomSC = (int)Math.random()*255;
-        int randomEC = (int)Math.random()*255;
-        int randomHC = (int)Math.random()*255;
-        int randomHS = (int)Math.random()*3;
+    //method to return the Var object
+    public Vars getObject(){
+        return varObject;
+    }
 
-        this.skinColor = randomSC;
-        this.eyeColor = randomEC;
-        this.hairColor = randomHC;
-        this.hairStyle = randomHS;
+    @Override
+    //first checks if random button is pressed
+    //then it sets the paint to the correct color values
+    //it will then draw the face, eyes, and hair
+    protected void onDraw(Canvas canvas){
+
+            //checking to see if random button is pressed
+            if(varObject.random){
+                randomButton();
+            }
+
+        skin.setColor(Color.rgb(varObject.redSkin, varObject.greenSkin, varObject.blueSkin));
+        skin.setStyle(Paint.Style.FILL);
+
+        eyes.setColor(Color.rgb(varObject.redEyes, varObject.greenEyes, varObject.blueEyes));
+        eyes.setStyle(Paint.Style.FILL);
+
+        hair.setColor(Color.rgb(varObject.redHair, varObject.greenHair, varObject.blueHair));
+        hair.setStyle(Paint.Style.FILL);
+
+
+            //draw face
+            canvas.drawCircle(1000.0f, 700.0f, 500.0f, skin);
+
+
+            //draw eyes
+            canvas.drawCircle(800.0f, 600.0f, 50.0f, eyes);
+            canvas.drawCircle(1200.0f, 600.0f, 50.0f, eyes);
+
+            //checking for what hair style is selected, then draw hair style
+            //long hair
+            if (varObject.style == 2) {
+                canvas.drawRect(550.0f, 200.0f, 1450.0f, 400.0f, hair);
+                canvas.drawRect(500.0f, 200.0f, 700.0f, 1000.0f, hair);
+                canvas.drawRect(1300.0f, 200.0f, 1500.0f, 1000.0f, hair);
+            }
+            //short hair
+            else if (varObject.style == 1) {
+                canvas.drawRect(550.0f, 200.0f, 1450.0f, 400.0f, hair);
+            }
+            //no hair
+            else  {
+                //nothing happens because bald is an option
+            }
+
 
 
     }
+    //randomises all vars
+    public void random(){
+        Random rand = new Random();
+        varObject.redHair = rand.nextInt(256);
+        varObject.greenHair = rand.nextInt(256);
+        varObject.blueHair = rand.nextInt(256);
+
+        varObject.redEyes= rand.nextInt(256);
+        varObject.greenEyes = rand.nextInt(256);
+        varObject.blueEyes = rand.nextInt(256);
+
+        varObject.redSkin = rand.nextInt(256);
+        varObject.greenSkin = rand.nextInt(256);
+        varObject.blueSkin = rand.nextInt(256);
+
+        varObject.style = rand.nextInt(3);
+    }
+
+
+    //randomises all the vars that will be used in making the face when random button is pressed
+    public void randomButton(){
+        random();
+        skin.setColor(Color.rgb(varObject.redSkin, varObject.greenSkin, varObject.blueSkin));
+        skin.setStyle(Paint.Style.FILL);
+
+        random();
+        eyes.setColor(Color.rgb(varObject.redEyes, varObject.greenEyes, varObject.blueEyes));
+        eyes.setStyle(Paint.Style.FILL);
+
+        random();
+        hair.setColor(Color.rgb(varObject.redHair, varObject.greenHair, varObject.blueHair));
+        hair.setStyle(Paint.Style.FILL);
+
+        varObject.random = false;
+    }
+
 
 
 }
--- app/src/main/java/com/example/homework1/MainActivity.java
+++ app/src/main/java/com/example/homework1/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.homework1;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 /**
  * Author: William Cloutier
- * Date: 9/20/20
- * Version: Homework A
+ * Date: 10/6/20
+ * Version: Homework B
+ * Bugs: if you click on a radio button and then click on another radio button the seekbar values from the first
+ * radio button will become the values of the second radio button. I have no idea why this is happening
  */
 
 
@@ -33,49 +42,105 @@ import android.widget.Toast;
  * Solution: I followed the SDK to learn how to implement a radio button and radio group
  */
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+/**
+ * External Citation
+ * Date:    10/6/20
+ * Problem:     understanding of how to draw on android studio was lacking
+ * Resource:
+ *      https://github.com/Xalen8242/BirthdayCake-master
+ * Solution: I used my lab from another class to guide my actions. This was very useful and it reminded me how to use onDraw
+ */
 
+
+//this will create my spinners, seekbars, buttons, and radio buttons
+//it will then use the creations, set them equal to the corresponding item in activity_main, and then put them into Controller.java
+//radiobutton has method to see if it is clicked or not
+public class MainActivity extends AppCompatActivity {
+    Vars radioObject;
     @Override
-    //onCreate is making a spinner that will use the hairSpinner id and contain the hairStyles text array
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
 
+        Face object = findViewById(R.id.face);
+        radioObject = object.getObject();
+        Controller controller = new Controller(object);
+
         Spinner spinner = (Spinner) findViewById(R.id.hairSpinner);
-        spinner.setOnItemSelectedListener(this);
+        spinner.setOnItemSelectedListener(controller);
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.hairStyles, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
+        Button rand = findViewById(R.id.randomFace);
+        rand.setOnClickListener(controller);
+
+        SeekBar redS = findViewById(R.id.red);
+        redS.setOnSeekBarChangeListener(controller);
+
+        SeekBar greenS = findViewById(R.id.green);
+        greenS.setOnSeekBarChangeListener(controller);
+
+        SeekBar blueS = findViewById(R.id.blue);
+        blueS.setOnSeekBarChangeListener(controller);
+
+
+
+
+
+
     }
 
 
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+    public void onRadioButtonClicked (View view){
+
+        boolean checked = ((RadioButton) view).isChecked();
+        SeekBar redS = findViewById(R.id.red);
+        SeekBar greenS = findViewById(R.id.green);
+        SeekBar blueS = findViewById(R.id.blue);
+
+        switch(view.getId()) {
+            case R.id.radioButtonHair:
+                    redS.setProgress(radioObject.redHair);
+                    greenS.setProgress(radioObject.greenHair);
+                    blueS.setProgress(radioObject.blueHair);
+
+                    radioObject.radioButton01 = true;
+                    radioObject.radioButton02 = false;
+                    radioObject.radioButton03 = false;
+
+                break;
+
+
+
+            case R.id.radioButtonEyes:
+                    redS.setProgress(radioObject.redEyes);
+                    greenS.setProgress(radioObject.greenEyes);
+                    blueS.setProgress(radioObject.blueEyes);
+
+                    radioObject.radioButton01 = false;
+                    radioObject.radioButton02 = true;
+                    radioObject.radioButton03 = false;
+                break;
+
+
+            case R.id.radioButtonSkin:
+                    redS.setProgress(radioObject.redSkin);
+                    greenS.setProgress(radioObject.greenSkin);
+                    blueS.setProgress(radioObject.blueSkin);
+
+                    radioObject.radioButton01 = false;
+                    radioObject.radioButton02 = false;
+                    radioObject.radioButton03 = true;
+                    break;
+                }
+
 
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-
-    }
-        //radio button method what checks to see if a button is checked and executes something when it is selected
-        public void onRadioButtonCLicked(View view){
-            boolean checked = ((RadioButton) view).isChecked();
-
-            switch(view.getId()){
-                case R.id.radioButtonHair:
-                    if(checked)
-                        //hello
-                        break;
-                case R.id.radioButtonEyes:
-                    if(checked)
-                        //hi
-                        break;
-                case R.id.radioButtonSkin:
-                    if(checked)
-                        //enter
-                        break;
-            }
-        }
 }
